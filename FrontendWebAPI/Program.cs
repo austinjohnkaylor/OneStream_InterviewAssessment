550252681a13f6ff95f3ff1d575a9828d278bca9
Austin Kaylor <austinjohnkaylor@outlook.com>
2023-08-24 19:33:10 -0400

    Adding the right line to add JwtBearer auth

--- FrontendWebAPI/Program.cs
+++ FrontendWebAPI/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 //     x.DefaultAuthenticateScheme = 
 // }); --> watch this video for more info -> https://www.youtube.com/watch?v=mgeuh8k3I4g
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
