b98526fbcf68ecad3280f43b4d6c08f3ac432510
Austin Kaylor <austinjohnkaylor@outlook.com>
2023-08-26 22:41:14 -0400

    Adding some entity type configurations for Location and Person

---
+++ EntityFramework/EntityTypeConfigurations/LocationEntityTypeConfiguration.cs
@@ -0,0 +1,43 @@
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityFramework.EntityTypeConfigurations;
+
+public class LocationEntityTypeConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.ToTable("Locations"); // Optional table name, you can change it as needed
+        builder.HasKey(l => l.Id);
+        builder.Property(l => l.Id).ValueGeneratedOnAdd();
+        builder.Property(l => l.Address1).IsRequired().HasMaxLength(100);
+        builder.Property(l => l.Address2).HasMaxLength(100);
+        builder.Property(l => l.City).IsRequired().HasMaxLength(50);
+        builder.Property(l => l.State).IsRequired().HasMaxLength(50);
+        builder.Property(l => l.ZipCode).IsRequired();
+
+        builder.HasData(new Location
+        {
+            Address1 = "555 Happy Place Drive",
+            Address2 = string.Empty,
+            City = "Happyville",
+            State = "Ohio",
+            ZipCode = 55555
+        }, new Location
+        {
+            Address1 = "123 Sycamore Tree Lane",
+            Address2 = string.Empty,
+            City = "Treehill",
+            State = "North Carolina",
+            ZipCode = 99999
+        }, new Location
+        {
+            Address1 = "456 Broken Sticks Avenue",
+            Address2 = string.Empty,
+            City = "Woods",
+            State = "Pennsylvania",
+            ZipCode = 11111
+        });
+    }
+}
---
+++ EntityFramework/EntityTypeConfigurations/PersonEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityFramework.EntityTypeConfigurations;
+
+public class PersonEntityTypeConfiguration : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("People"); // Optional table name, you can change it as needed
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id).ValueGeneratedOnAdd();
+        builder.Property(p => p.FirstName).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.MiddleName).HasMaxLength(50);
+        builder.Property(p => p.LastName).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.Age).IsRequired();
+        
+        builder.HasData(new Person
+        {
+            FirstName = "John",
+            MiddleName = "Cookie",
+            LastName = "Doe",
+            Age = 30
+        }, new Person
+        {
+            FirstName = "Jane",
+            MiddleName = "Cookie",
+            LastName = "Doe",
+            Age = 35
+        });
+    }
+}
